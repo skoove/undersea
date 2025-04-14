049144994fdd2e0761edf0706e8810521a1ccd8f
Zie Sturges <zie@sturges.com.au>
2025-04-14 11:01:54 +1000

    last junk commit

--- undersea-tui/src/main.rs
+++ undersea-tui/src/main.rs
@@ -1,37 +1,3 @@
-use ratatui::Frame;
-use ratatui::crossterm::event::{self, Event, KeyCode, KeyEvent};
-use ratatui::prelude::*;
-use ratatui::text::Text;
-
 fn main() -> std::io::Result<()> {
-    let mut terminal = ratatui::init();
-
-    loop {
-        terminal.draw(draw)?;
-
-        if let Event::Key(KeyEvent {
-            code: KeyCode::Char('q'),
-            ..
-        }) = event::read()?
-        {
-            break;
-        }
-    }
-
-    ratatui::restore();
     Ok(())
-}
-
-fn draw(frame: &mut Frame) {
-    let colors = ["red", "green", "yellow", "blue", "magenta", "cyan", "grey"];
-
-    let area = frame.area();
-    let mut y = area.y;
-
-    for color in colors {
-        let text = Text::styled("hewwowowowo", color.parse::<Color>().unwrap());
-        let widget_area = Rect::new(area.x, y, area.width, 1);
-        frame.render_widget(text, widget_area);
-        y += 1
-    }
 }
