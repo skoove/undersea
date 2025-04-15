3c9f6bcc9fa00b4e7deb4436b9024fefad89dcc2
Zie Sturges <zie@sturges.com.au>
2025-04-16 09:04:16 +1000

    lib: add methods to episode

--- undersea-lib/src/episode.rs
+++ undersea-lib/src/episode.rs
@@ -8,6 +8,40 @@ pub struct Episode {
     pub(crate) title: String,
     pub(crate) date: DateTime<Utc>,
     pub(crate) duration: Option<time::Duration>,
-    pub(crate) resume_time: Option<time::Duration>,
+    pub(crate) resume_time: time::Duration,
     pub(crate) finished: bool,
+}
+
+impl Episode {
+    /// Returns the url of the episodes attached media.
+    pub fn media_url(&self) -> &str {
+        &self.media_url
+    }
+
+    /// Returns the title of an episode.
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    /// Returns the ['DateTime'] of the episodes upload, in UTC.
+    pub fn date(&self) -> &DateTime<Utc> {
+        &self.date
+    }
+
+    /// Returns the total duration of the episode as a [`Duration`]. If this is none,
+    /// that means the duration has not been worked out yet, and you need to
+    /// get it.
+    pub fn duration(&self) -> &Option<time::Duration> {
+        &self.duration
+    }
+
+    /// Returns the time where the playback should be resumed.
+    pub fn resume_time(&self) -> &time::Duration {
+        &self.resume_time
+    }
+
+    /// Returns weather or not the episode has been finished.
+    pub fn finished(&self) -> bool {
+        self.finished
+    }
 }
